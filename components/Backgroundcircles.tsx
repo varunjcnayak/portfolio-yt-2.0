8b6eed4c1ab25688a7d5fd0e7c40e1d05cef5d01
K, Varun Nayak <varunnayak.k@ecolab.com>
2022-11-25 22:42:46 +0530

    commit

--- components/Backgroundcircles.tsx
+++ components/Backgroundcircles.tsx
@@ -14,7 +14,7 @@ function BackgroundCircles({}: Props) {
         opacity: [0.1, 0.2, 0.4, 0.8, 0.1, 1.0],
         borderRadius: ["20%", "20%", "50%", "80%", "20%"],
       }}
-      transition={{
+    transition={{
         duration: 2.5,
       }}
       className="relative flex justify-center items-center"
